8c90de57c2033e725f5744e0cb1a85b12cfe2adb
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2021-01-01 18:07:47 -0500

    header layout changed

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,32 +1,12 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../redux/rootReducer";
-import { logOutAction } from "../../redux/userReducer/actions";
-import Button from "../common/button.component";
+import { BsSun } from "react-icons/bs";
+import { GiMoon } from "react-icons/gi";
 import Logo from "../common/Logo.component";
-import { usePageChangeHandler } from "../../Hooks/usePageChangeHandler";
-import { currentChatWithAction } from "../../redux/userReducer/actions";
 import SearchBar from "./searchBar.component";
-import BtnMinimal from "../common/minimalBtn.component";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
-  const dispatch = useDispatch();
-  const { loggedIn, username } = useSelector((state: RootState) => state.user);
-  const { currentPage } = useSelector((state: RootState) => state.currentPage);
-
-  const {
-    regChangePageHandler,
-    homeChangePageHandler,
-    loginChangePageHandler,
-  } = usePageChangeHandler();
-
-  const LogoutHandler = () => {
-    dispatch(logOutAction());
-    dispatch(currentChatWithAction({ id: null, username: null }));
-  };
-
   return (
     <div
       className="w-full h-14 lg:h-14 3xl:h-14 
@@ -39,21 +19,14 @@ const Header: React.FC<HeaderProps> = () => {
       <Logo />
       <SearchBar />
       <div className="flex items-center justify-end">
-        {loggedIn ? (
-          <Button onclick={LogoutHandler} btnText={`${username} (Logout) `} />
-        ) : (
-          <>
-            {currentPage !== "HOME" && (
-              <BtnMinimal onclick={homeChangePageHandler} text={"home"} />
-            )}
-            {currentPage !== "LOGIN" && (
-              <BtnMinimal onclick={loginChangePageHandler} text="Login" />
-            )}
-            {currentPage !== "REGISTER" && (
-              <BtnMinimal onclick={regChangePageHandler} text={"Register"} />
-            )}
-          </>
-        )}
+        <BsSun
+          className="fill-current text-kar-blue w-6 h-6"
+          title="Light Mode"
+        />
+        <GiMoon
+          className="fill-current text-kar-blue w-6 h-6 ml-4"
+          title="Dark Mode"
+        />
       </div>
     </div>
   );
